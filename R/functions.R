bf5019fb60403ab6916132208ea1f2934a8a16b5
Erik Peteri <peteri.erik@gmail.com>
2024-03-14 18:26:17 +0200

    add routes for calculation

--- R/functions.R
+++ R/functions.R
@@ -1,7 +1,32 @@
 # functions.R
 
-#* @get /calculate
+#* @post /area_above
 #* @serializer unboxedJSON
-calculateProbability <- function() {
-  list('response' = "Hello world!")
+#* @param value:numeric
+#* @param mean:numeric
+#* @param sd:numeric
+areaAbove <- function(value, mean, sd) {
+  probability <- pnorm(as.numeric(value), as.numeric(mean), as.numeric(sd), lower.tail = FALSE)
+  list(probability)
+}
+
+#* @post /area_below
+#* @serializer unboxedJSON
+#* @param value:numeric
+#* @param mean:numeric
+#* @param sd:numeric
+areaBelow <- function(value, mean, sd) {
+  probability <- pnorm(as.numeric(value), as.numeric(mean), as.numeric(sd), lower.tail = TRUE)
+  list(probability)  
+}
+
+#* @post /area_between
+#* @serializer unboxedJSON
+#* @param value_lower:numeric
+#* @param value_upper:numeric
+#* @param mean:numeric
+#* @param sd:numeric
+areaBetween <- function(value_lower, value_upper, mean, sd) {
+  probability <-  pnorm(as.numeric(value_upper), as.numeric(mean), as.numeric(sd), lower.tail = TRUE) - pnorm(as.numeric(value_lower), as.numeric(mean), as.numeric(sd), lower.tail = TRUE)
+  
 }
